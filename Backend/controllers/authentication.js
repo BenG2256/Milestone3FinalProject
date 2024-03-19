1483e0b1fa7682be6288053efc9d1dc9290411c2
tamccullough88 <tamccullough88@gmail.com>
2024-03-18 21:23:42 -0400

    adding index.js and user models, authentication, index.js,

---
+++ Backend/controllers/authentication.js
@@ -0,0 +1,34 @@
+const router = require('express').Router()
+const db = require('../models')
+const bcrypt = require('bcrypt')
+const jwt = require('json-web-token')
+
+const { User } = db
+
+
+
+router.post('/', async (req, res) => {
+
+    let user = await User.findOne({
+        where: { email: req.body.email }
+    })
+
+    if (!user || !await bcrypt.compare(req.body.password, user.passwordDigest)) {
+        res.status(404).json({
+            message: `Could not find a user with the provided username and password`
+        })
+    } else {
+        const result = await jwt.encode(process.env.JWT_SECRET, { id: user.userId })
+        res.json({ user: user, token: result.value })
+    }
+})
+
+
+router.get('/profile', async (req, res) => {
+    res.json(req.currentUser)
+})
+
+
+
+
+module.exports = router
---
+++ Backend/index.js
@@ -0,0 +1,27 @@
+// Modules and Globals
+require('dotenv').config()
+const express = require('express')
+const bodyParser = require('body-parser')
+const cors = require('cors')
+const app = express();
+const defineCurrentUser = require('./middleware/defineCurrentUser')
+
+// Express Settings
+app.use(cors())
+app.use(express.static('public'))
+app.use(express.urlencoded({ extended: true }))
+app.use(bodyParser.json())
+app.use(defineCurrentUser)
+
+// Controllers & Routes
+
+app.use(express.urlencoded({ extended: true }))
+
+app.use('/places', require('./controllers/places'))
+app.use('/users', require('./controllers/users'))
+app.use('/authentication', require('./controllers/authentication'))
+
+// Listen for Connections
+app.listen(process.env.PORT, () => {
+    console.log(`Listening on ${process.env.PORT}`)
+})
