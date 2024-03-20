0e810f60bde818b1568aa31242209b6606e0d713
tamccullough88 <tamccullough88@gmail.com>
2024-03-20 18:41:19 -0400

    updated index.js in backend. reviews.js controller.

--- Backend/controllers/authentication.js
+++ Backend/controllers/authentication.js
@@ -1,11 +1,9 @@
 const router = require('express').Router()
-const db = require('../models')
+const db = require("../models")
 const bcrypt = require('bcrypt')
 const jwt = require('json-web-token')
 
 const { User } = db
-
-
 
 router.post('/', async (req, res) => {
 
@@ -14,21 +12,29 @@ router.post('/', async (req, res) => {
     })
 
     if (!user || !await bcrypt.compare(req.body.password, user.passwordDigest)) {
-        res.status(404).json({
-            message: `Could not find a user with the provided username and password`
-        })
+        res.status(404).json({ message: `Could not find a user with the provided username and password` })
     } else {
         const result = await jwt.encode(process.env.JWT_SECRET, { id: user.userId })
         res.json({ user: user, token: result.value })
     }
 })
 
-
 router.get('/profile', async (req, res) => {
-    res.json(req.currentUser)
+    try {
+        const [method, token] = req.headers.authorization.split(' ')
+        if (method == 'Bearer') {
+            const result = await jwt.decode(process.env.JWT_SECRET, token)
+            const { id } = result.value
+            let user = await User.findOne({
+                where: {
+                    userId: id
+                }
+            })
+            res.json(user)
+        }
+    } catch (err) {
+        res.json(null)
+    }
 })
-
-
-
 
 module.exports = router
--- Backend/index.js
+++ Backend/index.js
@@ -4,20 +4,20 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const cors = require('cors')
 const app = express();
-const defineCurrentUser = require('./middleware/defineCurrentUser')
+// const defineCurrentUser = require('./middleware/defineCurrentUser')
 
 // Express Settings
 app.use(cors())
 app.use(express.static('public'))
 app.use(express.urlencoded({ extended: true }))
 app.use(bodyParser.json())
-app.use(defineCurrentUser)
+// app.use(defineCurrentUser)
 
 // Controllers & Routes
 
 app.use(express.urlencoded({ extended: true }))
 
-app.use('/places', require('./controllers/places'))
+// app.use('/places', require('./controllers/places'))
 app.use('/users', require('./controllers/users'))
 app.use('/authentication', require('./controllers/authentication'))
 
