831d28bd530a06361f4b8176abe4f91282882cff
Ben Gilley <bengilley5@gmail.com>
2024-03-19 23:20:05 -0400

    added models and migration files for database, and config.js for use of environmental variables

--- Backend/index.js
+++ Backend/index.js
@@ -1,14 +1,3 @@
-require('dotenv').config()
-const express = require('express');
-const app = express();
-
-// Add middleware, routes, etc.
-
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
-
 // Modules and Globals
 require('dotenv').config()
 const express = require('express')
