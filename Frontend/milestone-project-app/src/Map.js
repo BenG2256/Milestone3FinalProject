3e31b7f6d7f5554d10611e8ccb54e2e553d6b484
Zach Fountain <141088304+ZachFount@users.noreply.github.com>
2024-03-20 20:55:33 -0400

    optimized api to pull place data

--- Frontend/milestone-project-app/src/Map.js
+++ Frontend/milestone-project-app/src/Map.js
@@ -17,28 +17,21 @@ const Map = () => {
       position => {
         const { latitude, longitude } = position.coords;
         map.setCenter([longitude, latitude]);
+
+        // Fetch nearby places using obtained location
+        const options = {
+          method: 'GET',
+          headers: {
+            accept: 'application/json',
+            Authorization: 'fsq3agO3EAI9tSf5SCoyd0czUKTwQZp22Tqy+f5fp1rR77M='
+          }
+        };
+        const url = `https://api.foursquare.com/v3/places/nearby?ll=${latitude},${longitude}`;
         
-        // Add a marker for the user's current location
-        new mapboxgl.Marker().setLngLat([longitude, latitude]).addTo(map);
-
-        // Search for nearby restaurants using Foursquare API
-        fetch(`https://api.foursquare.com/v2/venues/explore?client_id=VRTP2TAGBNTCOWTEETZQUZN2K5UXXDTNR05EHTNF4OLJS3O1&client_secret=2AUCMGX3KUYFNANS0CKZJYFKCSYREVRINHC5J4GP5K1UJMFZ
-
-        &v=20220315&ll=${latitude},${longitude}&query=restaurant&limit=10`)
+        fetch(url, options)
           .then(response => response.json())
-          .then(data => {
-            const restaurants = data.response.groups[0].items;
-            restaurants.forEach(restaurant => {
-              const { lat, lng } = restaurant.venue.location;
-              new mapboxgl.Marker()
-                .setLngLat([lng, lat])
-                .setPopup(new mapboxgl.Popup().setHTML(`<h3>${restaurant.venue.name}</h3>`))
-                .addTo(map);
-            });
-          })
-          .catch(error => {
-            console.error('Error searching for nearby restaurants:', error);
-          });
+          .then(response => console.log(response))
+          .catch(err => console.error(err));
       },
       error => {
         console.error('Error getting user location:', error);
