59eec44233e06bc373761f9d0444b0681d9c25cf
Zach Fountain <141088304+ZachFount@users.noreply.github.com>
2024-03-25 17:55:13 -0400

    add restult sect

--- Frontend/milestone-project-app/src/Map.js
+++ Frontend/milestone-project-app/src/Map.js
@@ -1,15 +1,17 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import mapboxgl from 'mapbox-gl';
 
 mapboxgl.accessToken = 'pk.eyJ1IjoiemFjaGZvdW50MSIsImEiOiJjbHUwZmZ6MGEwMm9qMmtycHNnb2ZhNnVmIn0.3iOk3GBPpQgZBnR0rA1b9A';
 
 const Map = () => {
+  const [restaurants, setRestaurants] = useState([]);
+
   useEffect(() => {
     const map = new mapboxgl.Map({
       container: 'map',
       style: 'mapbox://styles/mapbox/streets-v11',
       center: [0, 0], // Default center before obtaining user's location
-      zoom: 9
+      zoom: 15
     });
 
     const addMarkers = (coordinates, popupContent = null) => {
@@ -25,18 +27,17 @@ const Map = () => {
         const { latitude, longitude } = position.coords;
         map.setCenter([longitude, latitude]);
 
-        
         // Add a marker for the user's current location
         addMarkers([longitude, latitude], new mapboxgl.Popup().setHTML('<h3>Your Location</h3>'));
 
         // Search for nearby restaurants using Foursquare API
         fetch(`https://api.foursquare.com/v2/venues/explore?client_id=VRTP2TAGBNTCOWTEETZQUZN2K5UXXDTNR05EHTNF4OLJS3O1&client_secret=2AUCMGX3KUYFNANS0CKZJYFKCSYREVRINHC5J4GP5K1UJMFZ
-
         &v=20220315&ll=${latitude},${longitude}&query=restaurant&limit=10`)
           .then(response => response.json())
           .then(data => {
-            const restaurants = data.response.groups[0].items;
-            restaurants.forEach(restaurant => {
+            const restaurantsData = data.response.groups[0].items;
+            setRestaurants(restaurantsData);
+            restaurantsData.forEach(restaurant => {
               const { lat, lng } = restaurant.venue.location;
               new mapboxgl.Marker()
                 .setLngLat([lng, lat])
@@ -48,22 +49,6 @@ const Map = () => {
             console.error('Error searching for nearby restaurants:', error);
           });
 
-
-        // Fetch nearby places using obtained location
-        const options = {
-          method: 'GET',
-          headers: {
-            accept: 'application/json',
-            Authorization: 'fsq3agO3EAI9tSf5SCoyd0czUKTwQZp22Tqy+f5fp1rR77M='
-          }
-        };
-        const url = `https://api.foursquare.com/v3/places/nearby?ll=${latitude},${longitude}`;
-        
-        fetch(url, options)
-          .then(response => response.json())
-          .then(response => console.log(response))
-          .catch(err => console.error(err));
-
       },
       error => {
         console.error('Error getting user location:', error);
@@ -73,7 +58,19 @@ const Map = () => {
     return () => map.remove();
   }, []);
 
-  return <div id="map" style={{ width: '100%', height: '400px' }} />;
+  return (
+    <div>
+      <div id="map" style={{ width: '100%', height: '400px' }} />
+      <div>
+        <h2>Restaurants Near You:</h2>
+        <ul>
+          {restaurants.map(restaurant => (
+            <li key={restaurant.venue.id}>{restaurant.venue.name}</li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
 };
 
 export default Map;
--- Frontend/milestone-project-app/src/index.js
+++ Frontend/milestone-project-app/src/index.js
@@ -3,11 +3,12 @@ import ReactDOM from 'react-dom/client';
 import './scss/style.css';
 import App from './App';
 import Login from './components/login'
+import Map from './Map';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Login />
+    <Map />
   </React.StrictMode>
 );
 
