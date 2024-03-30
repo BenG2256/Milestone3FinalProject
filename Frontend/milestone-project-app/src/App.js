e5e92dcb60eba379dad84d5d8bf9754eee5ef082
Thomas McCullough <tamccullough88@gmail.com>
2024-03-30 12:27:10 -0400

    updated routes

--- Frontend/milestone-project-app/src/App.js
+++ Frontend/milestone-project-app/src/App.js
@@ -1,23 +1,24 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import './scss/style.css';
-import Home from './components/home';
+
 import Login from './components/user/login';
-import Map from './components/Map';
+
 import NavBar from './components/NavBar'
 import CurrentUserProvider from './contexts/CurrentUser'
 import SignUp from './components/user/register'
 
 import HomePage from './pages/HomePage';
-import LoginPage from './pages/LoginPage'
 
 
 function App() {
   return (
     <CurrentUserProvider>
       <BrowserRouter>
+        <NavBar />
         <Routes>
-          <Route path='/' element={<HomePage />} />
-          <Route path='/login' element={<LoginPage />} />
+          <Route path="/login" element={<Login />} />
+          <Route exact path="/" element={<HomePage />} />
+          <Route exact path='/signup' element={<SignUp />} />
         </Routes>
       </BrowserRouter>
     </CurrentUserProvider>
--- Frontend/milestone-project-app/src/pages/HomePage.jsx
+++ Frontend/milestone-project-app/src/pages/HomePage.jsx
@@ -1,10 +1,9 @@
-import NavBar from "../components/NavBar"
+
 import Map from "../components/Map"
 
 function HomePage() {
     return (
         <div className="App-body">
-            <NavBar />
             <Map />
         </div>
     )
